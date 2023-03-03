239d66d8908d13a093e35173ca9d1f77a145b584
DenysenkoSerhii <107317701+DenysenkoSerhii@users.noreply.github.com>
2023-03-03 15:27:06 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Provider } from "react-redux";
+
+
+import MyContacts from "./modules/MyContacts";
+
+import store from "./redux/store";
+
+function App() {
+  return (
+    <Provider store={store}>
+        <MyContacts />
+    </Provider>
+
+  );
+}
+
+export default App;
